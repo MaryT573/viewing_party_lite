011ffade7ac8f26a6cca0bd742460635ae966447
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-08-22 20:35:46 -0600

    Update spec files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :events, dependent: :destroy
+  has_many :events_users, through: :events, dependent: :destroy
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Users, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:events) }
+  it { should have_many(:events_users) }
 end
