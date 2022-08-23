614cbf2d947f867a7a9ea443fb579cac78170354
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-08-22 19:46:06 -0600

    Update eventUsers; add role

--- db/migrate/20220822222419_create_attendees.rb
+++ db/migrate/20220822222419_create_attendees.rb
@@ -3,6 +3,7 @@ class CreateEventsUsers < ActiveRecord::Migration[5.2]
     create_table :eventsusers do |t|
       t.references :user, foreign_key: true
       t.references :event, foreign_key: true
+      t.integer :role, default: 0
 
       t.timestamps
     end
