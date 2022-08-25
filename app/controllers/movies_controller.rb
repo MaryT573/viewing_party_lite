53b523f7e270dbb32b616aa4756f088d3894a2cb
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-08-24 20:00:05 -0600

    update views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,15 @@
 class MoviesController < ApplicationController
   def index; end
 
-  def show
-    @movies = MovieFacade.search_movies(params[:search])
+  def search 
+    # @movies = MovieFacade.search_movies(params[:search]) 
   end
 
-  def search
-    @movies = MovieFacade.search_movies(params[:search])
-    render search_movies_path
+  def show
+    if params[:search]
+      @movies = MovieFacade.search_movies(params[:search])
+    else
+      @movies = MovieFacade.search_trending
+    end
   end
 end
