0a6270fced1c38a79fc0cb507e17b876f29a6519
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-25 15:41:43 -0600

    changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def index
+    # require 'pry', binding.pry
+    
     @user = User.find(params[:id])
     @url = "https://api.themoviedb.org"
   end 
@@ -12,6 +14,7 @@ class MoviesController < ApplicationController
   end
 
   def search
+    # require 'pry', binding.pry
     @user = User.find(params[:id])
     @url = "https://image.tmdb.org/t/p/w500"
     @movies = MovieFacade.search_movies(params[:search], params[:page])
@@ -19,7 +22,12 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    #require 'pry', binding.pry
+    # @movie = @movies.select {|m| m.id = params[:movie_id] }
+    @movie1 = movie_poro(movie_params)
+    # @movie = Movie.new(movie_params)
+    require 'pry'; binding.pry 
+    # @movie = MovieFacade.search_movies(params[:movie][:title])
   end
 
   def new
@@ -39,6 +47,7 @@ class MoviesController < ApplicationController
 
   private
   def movie_params
-    params.permit(:title, :release_date, :overview, :image_url, :average_rating, :genre, :runtime)
+    params.permit(:movie)
+    #params.permit(:title, :release_date, :overview, :image_url, :average_rating, :genre, :runtime)
   end
 end
